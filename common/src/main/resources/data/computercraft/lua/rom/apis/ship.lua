f163bc2c0e39493f7b8aaa84d3682a7334ecf9a2
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-11-25 22:50:03 -0600

    Halp

--- common/src/main/resources/data/computercraft/lua/rom/apis/ship.lua
+++ common/src/main/resources/data/computercraft/lua/rom/apis/ship.lua
@@ -135,7 +135,9 @@
 
 --- Extended Ship API
 --
--- This section contains extended functions for the Ship API that provide additional functionality beyond the basic Ship operations. As of CC: VS 0.5.0, these functions have been rolled into the main Ship API for better usability.
+-- This section describes additional methods available in the Ship API for advanced manipulation of the Ship.
+-- These methods may require special permissions or configurations to use.
+-- As of CC: VS 0.5.0, these methods are merged into the main Ship API and are available on all computers with Ship access.
 -- @section extended_ship_api
 
 --- Sets the Ship to be static or dynamic
@@ -229,7 +231,8 @@
 
 --- Deprecated Methods
 --
--- The following methods have been deprecated and are no longer available. Please refer to the updated Ship API documentation for alternative methods.
+-- The following methods have been deprecated and are no longer available. 
+-- Please refer to the updated Ship API documentation for alternative methods.
 -- @section deprecated
 
 --- Gets the Ship's Euler Angles in ZYX order
