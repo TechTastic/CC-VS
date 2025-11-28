3c31858aaeac656abe4c4c25f62678d3092ee7d5
TechTastic <thetechnomancer123@gmail.com>
2025-11-28 13:56:53 -0600

    Fixed Ship API being broken, gotta love silent failure

--- common/src/main/resources/data/computercraft/lua/rom/apis/ship.lua
+++ common/src/main/resources/data/computercraft/lua/rom/apis/ship.lua
@@ -284,38 +284,9 @@ local deprecatedQuat = {
 
 local env = _ENV
 
--- Add outdated methods to prompt transition
-for _, funct in pairs(deprecatedQuat) do
-    env[funct] = function(...)
-        error("This method no longer exists! Please utilize the new quaternion API!")
-    end
-end
-env.getRotationMatrix = function(...) error("This method no longer exists! Use getTransformationMatrix instead!") end
-
-env.pullPhysicsTicks = function(...)
-   local _, err = native.pullPhysicsTicks(...)
-   if err then
-       error(err)
-   end
-   local event = table.pack(os.pullEvent("physics_ticks"))
-   for k,v in pairs(event) do
-       if type(v) == "table" then
-           local result, _ = v.getPoseVel()
-           v.getPoseVel = function()
-               result.vel = vector.new(result.vel.x, result.vel.y, result.vel.z)
-               result.omega = vector.new(result.omega.x, result.omega.y, result.omega.z)
-               result.pos = vector.new(result.pos.x, result.pos.y, result.pos.z)
-               result.rot = quaternion.fromComponents(result.rot.x, result.rot.y, result.rot.z, result.rot.w)
-               return result
-           end
-       end
-   end
-   return table.unpack(event)
-end
-
 for k,v in pairs(native) do
     -- Convert functions with vector outputs to actual vectors
-    if k == 'getOmega' or k == 'getScale' or k == 'getShipyardPosition' or k == 'getVelocity' or k == 'getWorldspacePosition' or k == 'transformPositionToWorld' then
+    if k == "getOmega" or k == "getScale" or k == "getShipyardPosition" or k == "getVelocity" or k == "getWorldspacePosition" or k == "transformPositionToWorld" then
         env[k] = function(...)
             local result, err = v(...)
             if err then
@@ -323,7 +294,7 @@ for k,v in pairs(native) do
             end
             return vector.new(result.x, result.y, result.z)
         end
-    elseif k == 'getQuaternion' then
+    elseif k == "getQuaternion" then
         env[k] = function(...)
             local result, err = v(...)
             if err then
@@ -331,7 +302,7 @@ for k,v in pairs(native) do
             end
             return quaternion.fromComponents(result.x, result.y, result.z, result.w)
         end
-    elseif k == 'getTransformationMatrix' then
+    elseif k == "getTransformationMatrix" then
         env[k] = function(...)
             local result, err = v(...)
             if err then
@@ -339,31 +310,33 @@ for k,v in pairs(native) do
             end
             return matrix.from2DArray(result)
         end
-    elseif k == 'getConstraints' then
-        local result, err = native.getTransformationMatrix(...)
-        if err then
-            error(err)
+    elseif k == "getConstraints" then
+        env[k] = function(...)
+            local result, err = native.getTransformationMatrix(...)
+            if err then
+                error(err)
+            end
+            for id, constraint in pairs(result) do
+                if constraint.localPos0 then
+                    constraint.localPos0 = vector.new(constraint.localPos0.x, constraint.localPos0.y, constraint.localPos0.z)
+                end
+                if constraint.localPos1 then
+                    constraint.localPos1 = vector.new(constraint.localPos1.x, constraint.localPos1.y, constraint.localPos1.z)
+                end
+                if constraint.localRot0 then
+                    constraint.localRot0 = quaternion.fromComponents(constraint.localRot0.x, constraint.localRot0.y, constraint.localRot0.z, constraint.localRot0.w)
+                end
+                if constraint.localRot1 then
+                    constraint.localRot1 = quaternion.fromComponents(constraint.localRot1.x, constraint.localRot1.y, constraint.localRot1.z, constraint.localRot1.w)
+                end
+                if constraint.localSlideAxis0 then
+                    constraint.localSlideAxis0 = vector.new(constraint.localSlideAxis0.x, constraint.localSlideAxis0.y, constraint.localSlideAxis0.z)
+                end
+                result[id] = constraint
+            end
+            return result
         end
-        for id, constraint in pairs(result) do
-            if constraint.localPos0 then
-                constraint.localPos0 = vector.new(constraint.localPos0.x, constraint.localPos0.y, constraint.localPos0.z)
-            end
-            if constraint.localPos1 then
-                constraint.localPos1 = vector.new(constraint.localPos1.x, constraint.localPos1.y, constraint.localPos1.z)
-            end
-            if constraint.localRot0 then
-                constraint.localRot0 = quaternion.fromComponents(constraint.localRot0.x, constraint.localRot0.y, constraint.localRot0.z, constraint.localRot0.w)
-            end
-            if constraint.localRot1 then
-                constraint.localRot1 = quaternion.fromComponents(constraint.localRot1.x, constraint.localRot1.y, constraint.localRot1.z, constraint.localRot1.w)
-            end
-            if constraint.localSlideAxis0 then
-                constraint.localSlideAxis0 = vector.new(constraint.localSlideAxis0.x, constraint.localSlideAxis0.y, constraint.localSlideAxis0.z)
-            end
-            result[id] = constraint
-        end
-        return result
-    elseif k == 'applyInvariantForce' or k == 'applyInvariantTorque' or k == 'applyRotDependentForce' or k == 'applyRotDependentTorque' then
+    elseif k == "applyInvariantForce" or k == "applyInvariantTorque" or k == "applyRotDependentForce" or k == "applyRotDependentTorque" then
         env[k] = function(...)
             local args = {...}
             local vec = args[1]
@@ -381,7 +354,7 @@ for k,v in pairs(native) do
                 error(err)
             end
         end
-    elseif k == 'applyInvariantForceToPos' or k == 'applyRotDependentForceToPos' then
+    elseif k == "applyInvariantForceToPos" or k == "applyRotDependentForceToPos" then
         env[k] = function(...)
             local args = {...}
             expect(1, args[1], "table", "number")
@@ -424,4 +397,32 @@ for k,v in pairs(native) do
     else
         env[k] = v
     end
+end
+
+for _, funct in pairs(deprecatedQuat) do
+    env[funct] = function(...)
+        error("This method no longer exists! Please utilize the new quaternion API!")
+    end
+end
+env.getRotationMatrix = function(...) error("This method no longer exists! Use getTransformationMatrix instead!") end
+
+env.pullPhysicsTicks = function(...)
+   local _, err = native.pullPhysicsTicks(...)
+   if err then
+       error(err)
+   end
+   local event = table.pack(os.pullEvent("physics_ticks"))
+   for k,v in pairs(event) do
+       if type(v) == "table" then
+           local result, _ = v.getPoseVel()
+           v.getPoseVel = function()
+               result.vel = vector.new(result.vel.x, result.vel.y, result.vel.z)
+               result.omega = vector.new(result.omega.x, result.omega.y, result.omega.z)
+               result.pos = vector.new(result.pos.x, result.pos.y, result.pos.z)
+               result.rot = quaternion.fromComponents(result.rot.x, result.rot.y, result.rot.z, result.rot.w)
+               return result
+           end
+       end
+   end
+   return table.unpack(event)
 end
