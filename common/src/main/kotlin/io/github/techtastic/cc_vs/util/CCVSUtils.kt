5f7c310982f863e6076bc180176c176c03b3460c
TechTastic <74630543+TechTastic@users.noreply.github.com>
2025-12-02 16:15:53 -0600

    Finished Joints -> Lua

--- common/src/main/kotlin/io/github/techtastic/cc_vs/util/CCVSUtils.kt
+++ common/src/main/kotlin/io/github/techtastic/cc_vs/util/CCVSUtils.kt
@@ -1,9 +1,13 @@
 package io.github.techtastic.cc_vs.util
 
 import dan200.computercraft.api.lua.LuaException
-import io.github.techtastic.cc_vs.CCVSMod
 import org.joml.*
 import org.valkyrienskies.core.internal.joints.*
+import kotlin.Any
+import kotlin.Double
+import kotlin.Pair
+import kotlin.String
+import kotlin.to
 
 object CCVSUtils {
     fun Vector3dc.toLua() = mapOf(
@@ -30,13 +34,26 @@ object CCVSUtils {
         return tensor
     }
 
-    fun getComputerByID(id: Int) = CCVSMod.context.registry().computers.find { computer -> computer.id == id }
-
     fun VSJointAndId.toLua() = mapOf(Pair("id", this.jointId), Pair("constraint", this.joint.toLua()))
 
     fun VSJointPose.toLua() = mapOf("pos" to this.pos.toLua(), "rot" to this.rot.toLua())
 
+    fun VSD6Joint.LinearLimit.toLua() = mapOf(
+        "extent" to this.extent.toDouble(),
+        "stiffness" to this.stiffness?.toDouble(),
+        "damping" to this.damping?.toDouble()
+    )
+
     fun VSD6Joint.LinearLimitPair.toLua() = mapOf(
+        "lowerLimit" to this.lowerLimit.toDouble(),
+        "upperLimit" to this.upperLimit.toDouble(),
+        "restitution" to this.restitution?.toDouble(),
+        "bounceThreshold" to this.bounceThreshold?.toDouble(),
+        "stiffness" to this.stiffness?.toDouble(),
+        "damping" to this.damping?.toDouble()
+    )
+
+    fun VSD6Joint.AngularLimitPair.toLua() = mapOf(
         "lowerLimit" to this.lowerLimit.toDouble(),
         "upperLimit" to this.upperLimit.toDouble(),
         "restitution" to this.restitution?.toDouble(),
@@ -52,6 +69,51 @@ object CCVSUtils {
         "bounceThreshold" to this.bounceThreshold?.toDouble(),
         "stiffness" to this.stiffness?.toDouble(),
         "damping" to this.damping?.toDouble()
+    )
+
+    fun VSD6Joint.LimitPyramid.toLua() = mapOf(
+        "yLimitAngleMin" to this.yLimitAngleMin.toDouble(),
+        "yLimitAngleMax" to this.yLimitAngleMax.toDouble(),
+        "zLimitAngleMin" to this.zLimitAngleMin.toDouble(),
+        "zLimitAngleMax" to this.zLimitAngleMax.toDouble(),
+        "restitution" to this.restitution?.toDouble(),
+        "bounceThreshold" to this.bounceThreshold?.toDouble(),
+        "stiffness" to this.stiffness?.toDouble(),
+        "damping" to this.damping?.toDouble()
+    )
+
+    fun VSD6Joint.D6JointDrive.toLua() = mapOf(
+        "driveStiffness" to this.driveStiffness,
+        "driveDamping" to this.driveDamping,
+        "driveForceLimit" to this.driveForceLimit,
+        "isAcceleration" to this.isAcceleration
+    )
+
+    fun VSD6Joint.DrivePosition.toLua() = mapOf(
+        "pose" to this.pose.toLua(),
+        "autoWake" to this.autoWake
+    )
+
+    fun VSD6Joint.DriveVelocity.toLua() = mapOf(
+        "linear" to this.linear[Vector3d()].toLua(),
+        "angular" to this.angular[Vector3d()].toLua(),
+        "autoWake" to this.autoWake
+    )
+
+    fun VSD6Joint.Hinges.toLua() = mapOf(
+        "hinge0" to this.hinge0,
+        "hinge1" to this.hinge1
+    )
+
+    fun VSRevoluteJoint.VSRevoluteDriveVelocity.toLua() = mapOf(
+        "velocity" to this.velocity.toDouble(),
+        "autoWake" to this.autoWake
+    )
+
+    fun VSRackAndPinionJoint.VSRackAndPinionJointData.toLua() = mapOf(
+        "rackTeeth" to this.rackTeeth,
+        "pinionTeeth" to this.pinionTeeth,
+        "rackLength" to this.rackLength
     )
 
     fun VSJoint.toLua(): Map<String, Any?> {
@@ -76,18 +138,32 @@ object CCVSUtils {
         } else if (this is VSSphericalJoint) {
             constraint["limitCone"] = this.limitCone?.toLua()
         } else if (this is VSRevoluteJoint) {
-            TODO("VSRevoluteJoint to Lua")
+            constraint["angularLimitPair"] = this.angularLimitPair?.toLua()
+            constraint["driveVelocity"] = this.driveVelocity?.toLua()
+            constraint["driveForceLimit"] = this.driveForceLimit?.toDouble()
+            constraint["driveGearRatio"] = this.driveGearRatio?.toDouble()
+            constraint["driveFreeSpin"] = this.driveFreeSpin
         } else if (this is VSGearJoint) {
-            TODO("VSGearJoint to Lua")
+            constraint["hinges"] = this.hinges?.toLua()
+            constraint["gearRatio"] = this.gearRatio?.toDouble()
         } else if (this is VSRackAndPinionJoint) {
-            TODO("VSRackAndPinionJoint to Lua")
+            constraint["hinges"] = this.hinges?.toLua()
+            constraint["ratio"] = this.ratio?.toDouble()
+            constraint["data"] = this.data?.toLua()
         } else if (this is VSD6Joint) {
-            TODO("VSD6Joint to Lua")
+            constraint["motions"] = this.motions?.mapKeys { (axis, motion) -> axis.name }?.mapValues { (axis, motion) -> motion.name }
+            constraint["distanceLimit"] = this.distanceLimit?.toLua()
+            constraint["linearLimits"] = this.linearLimits?.mapKeys { (axis, pair) -> axis.name }?.mapValues { (axis, pair) -> pair.toLua() }
+            constraint["twistLimit"] = this.twistLimit?.toLua()
+            constraint["swingLimit"] = this.swingLimit?.toLua()
+            constraint["pyramidSwingLimit"] = this.pyramidSwingLimit?.toLua()
+            constraint["drives"] = this.drives?.mapKeys { (d, j) -> d.name }?.mapValues { (d, j) -> j.toLua() }
+            constraint["drivePosition"] = this.drivePosition?.toLua()
+            constraint["driveVelocity"] = this.driveVelocity?.toLua()
         }
 
         return constraint
     }
-
 
     fun Map<*, *>.toVector(): Vector3dc {
         val posTable = this as? Map<String, Double>
